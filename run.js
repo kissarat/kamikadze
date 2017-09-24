9f464b03849684f90454ae2c09005acc4900e4d2
Labiak Taras kissarat@gmail.com <kissarat@gmail.com>
2017-09-24 11:28:18 +0300

    + --channel CLI last argument

--- run.js
+++ run.js
@@ -1,6 +1,13 @@
 const request = require('request-promise')
 const {interval, delay, youtube: {all, search, videos}} = require('./config')
 
+let channelId
+const rex = /^--channel=(.*)$/.exec(process.argv[process.argv.length - 1])
+if (rex) {
+  channelId = rex[1]
+  console.log('CHANNEL: ' + channelId)
+}
+
 function youtubeCall(method, options, prefix = '') {
   return request({
     uri: prefix + 'https://www.googleapis.com/youtube/v3/' + method,
@@ -20,7 +27,11 @@ module.exports = async function run() {
     const r = await youtubeCall('search', search)
     const ids = r.items.map(item => item.id.videoId)
     for(const id of ids) {
-      await youtubeCall('videos', Object.assign({id}, videos), 'https://web.archive.org/save/')
+      const options = Object.assign({id}, videos)
+      if (channelId) {
+        options.channelId = channelId
+      }
+      await youtubeCall('videos', options, 'https://web.archive.org/save/')
       console.log(new Date().toISOString(), id)
       await timeoutPromise()
     }
